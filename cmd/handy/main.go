04adef80bef604db09281ed184f9bf54fea0b807
dmars8047 <dmars8047@live.com>
2024-12-19 19:06:26 -0500

    added quality and enc params + local config

--- cmd/handy/main.go
+++ cmd/handy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -14,12 +15,50 @@ import (
 )
 
 const (
-	INPUT_DIR_NAME  = "Input"
-	OUTPUT_DIR_NAME = "Output"
+	INPUT_DIR_NAME   = "Input"
+	OUTPUT_DIR_NAME  = "Output"
+	DEFAULT_QUALITY  = 20     // 18
+	DEFAULT_ENCODER  = "h264" //nvenc_h264
+	CONFIG_FILE_NAME = ".handy_config.json"
 )
 
+var possibleEncoderValues map[string]struct{} = map[string]struct{}{
+	"svt_av1":          {},
+	"svt_av1_10bit":    {},
+	"x264":             {},
+	"x264_10bit":       {},
+	"nvenc_h264":       {},
+	"x265":             {},
+	"x265_10bit":       {},
+	"x265_12bit":       {},
+	"nvenc_h265":       {},
+	"nvenc_h265_10bit": {},
+	"mpeg4":            {},
+	"mpeg2":            {},
+	"VP8":              {},
+	"VP9":              {},
+	"VP9_10bit":        {},
+	"theora":           {},
+}
+
 func main() {
-	setupFlag := flag.Bool("s", false, "Setup. Creates the input and output directory. Will not overwrite if they exist.")
+	var quality int
+	var encoder string
+
+	setupFlag := flag.Bool("s", false, fmt.Sprintf(`Setup. 
+	Creates the %s and %s directory as well as a %s file with sensible defaults in the current working directory. 
+	Will not overwrite if they exist.`, INPUT_DIR_NAME, OUTPUT_DIR_NAME, CONFIG_FILE_NAME))
+
+	listFlag := flag.Bool("l", false, `List Encoders. 
+	Displays a list of all valid encoder options.`)
+
+	flag.IntVar(&quality, "q", 0, fmt.Sprintf(`Quality. 
+	Sets the quality value to be used for each encoding task. If not provided then the value will be read from the '%s' file. 
+	If the config file cannot be found then then a default value of '%d' will be used.`, CONFIG_FILE_NAME, DEFAULT_QUALITY))
+
+	flag.StringVar(&encoder, "e", "", fmt.Sprintf(`Encoder. 
+	If not provided then the value will be read from the '%s' file. 
+	If the config file cannot be found then then a default value of '%s' will be used.`, CONFIG_FILE_NAME, DEFAULT_ENCODER))
 
 	flag.Parse()
 
@@ -34,8 +73,43 @@ func main() {
 			return
 		}
 
+		fmt.Println("Creating config file...")
+
+		setupErr = createConfigFile()
+
+		if setupErr != nil {
+			fmt.Printf("An error occurred while setting up the config file.\nError: %v\n", setupErr)
+			return
+		}
+
 		fmt.Printf("Directory setup complete.\n")
 		return
+	}
+
+	list := *listFlag
+
+	if list {
+		fmt.Println("Valid encoders:")
+		for k := range possibleEncoderValues {
+			fmt.Printf("%s\n", k)
+		}
+
+		fmt.Printf("\nNote. This is a list of all possible values. Your installation may or may not be configured with all of these.\n")
+		return
+	}
+
+	if encoder != "" {
+		_, encoderAllowed := possibleEncoderValues[encoder]
+
+		if !encoderAllowed {
+			fmt.Printf("The provided encoder '%s' does not match any known encoder. Use handy -l to get a list of all valid encoder options.\n", encoder)
+			return
+		}
+	}
+
+	if quality < 0 {
+		fmt.Printf("Invalid quality value detected. Setting to default value - %d\n", DEFAULT_QUALITY)
+		quality = DEFAULT_QUALITY
 	}
 
 	_, err := exec.LookPath("HandBrakeCLI")
@@ -44,6 +118,30 @@ func main() {
 		fmt.Println("HandBrakeCLI not found in $PATH. Please install the HandBrakeCLI. Exiting.")
 		return
 	}
+
+	config, err := readConfig()
+
+	if err != nil {
+		fmt.Printf("An error occurred while attempting to read from the config file.\nError: %v\n", err)
+	}
+
+	fmt.Println("################################")
+	fmt.Println("Handy Task Parameter Values")
+
+	// If not provided arg values to config values
+	if quality == 0 {
+		quality = config.Quality
+	}
+
+	fmt.Printf("Quality: %d", quality)
+
+	if encoder == "" {
+		encoder = config.Encoder
+	}
+
+	fmt.Printf("Encoder: %s", encoder)
+
+	fmt.Println("################################")
 
 	inputDirInfo, err := os.Stat(fmt.Sprintf("./%s", INPUT_DIR_NAME))
 
@@ -96,20 +194,27 @@ func main() {
 		}
 	}
 
-	cumulativeStartTime := time.Now()
+	totalBytesSaved := int64(0)
+	overallStartTime := time.Now()
 
 	for _, mkvFile := range mkvDirEntries {
 
-		outputFileRelPath := fmt.Sprintf("./%s/%s", outputDirInfo.Name(), mkvFile.Name())
+		inputFileInfo, err := mkvFile.Info()
 
-		fmt.Printf("Encoding: %s\n", mkvFile.Name())
+		if err != nil {
+			fmt.Printf("An error occurred when reading input file info from disk. Aborting remaining encoding tasks.\nError:  %v\n", err)
+		}
+
+		outputFileRelPath := fmt.Sprintf("./%s/%s", outputDirInfo.Name(), inputFileInfo.Name())
+
+		fmt.Printf("Encoding: %s\n", inputFileInfo.Name())
 
 		// HandBrakeCLI --input 50_First_Dates.mkv --output output.mkv --encoder nvenc_h264 --quality 18 --subtitle-lang-list English --all-subtitles --audio-lang-list English --all-audio
 		handbrakeCommand := exec.Command("HandBrakeCLI",
-			"--input", fmt.Sprintf("./%s/%s", inputDirInfo.Name(), mkvFile.Name()),
+			"--input", fmt.Sprintf("./%s/%s", inputDirInfo.Name(), inputFileInfo.Name()),
 			"--output", outputFileRelPath,
-			"--encoder", "nvenc_h264",
-			"--quality", "18",
+			"--encoder", encoder,
+			"--quality", fmt.Sprintf("%d", quality),
 			"--subtitle-lang-list", "eng,jpn",
 			"--all-subtitles",
 			"--audio-lang-list", "eng",
@@ -160,19 +265,129 @@ func main() {
 			return
 		}
 
+		outputSize, err := getFileSize(outputFileAbsPath)
+
+		if err != nil {
+			fmt.Printf("An error occurred when reading output file info from disk. Aborting remaining encoding tasks.\nError:  %v\n", err)
+		}
+
+		bytesSaved := inputFileInfo.Size() - outputSize
+		totalBytesSaved += bytesSaved
+
 		fmt.Printf("Encode Complete: Output file - %s\n", outputFileAbsPath)
-		fmt.Printf("Time Elapsed: %s\n", timeElapsedString(encodeDuration))
+		fmt.Printf("Time Elapsed: %s\n", formatTimeElapsedString(encodeDuration))
+		fmt.Printf("Saved Space: %s\n", formatSavedSpace(bytesSaved))
 	}
 
-	cumulativeDuration := time.Since(cumulativeStartTime).Round(time.Second)
+	totalDuration := time.Since(overallStartTime).Round(time.Second)
 	fmt.Printf("\nEncoding Tasks Complete: See results in the '%s' directory.\n", outputDirInfo.Name())
-	fmt.Printf("Total Time Elapsed: %s\n", timeElapsedString(cumulativeDuration))
+	fmt.Printf("Total Time Elapsed: %s\n", formatTimeElapsedString(totalDuration))
+	fmt.Printf("Total Saved Space: %s\n", formatSavedSpace(totalBytesSaved))
 }
 
-func timeElapsedString(m time.Duration) string {
+// Formats the given size in bytes into a human-readable string (GB, MB, KB, or Bytes).
+func formatSavedSpace(bytes int64) string {
+	const (
+		KB = 1024
+		MB = KB * 1024
+		GB = MB * 1024
+	)
+
+	switch {
+	case bytes >= GB:
+		return fmt.Sprintf("%.2f GB", float64(bytes)/float64(GB))
+	case bytes >= MB:
+		return fmt.Sprintf("%.2f MB", float64(bytes)/float64(MB))
+	case bytes >= KB:
+		return fmt.Sprintf("%.2f KB", float64(bytes)/float64(KB))
+	default:
+		return fmt.Sprintf("%d Bytes", bytes)
+	}
+}
+
+func formatTimeElapsedString(m time.Duration) string {
 	minutes := int(math.Floor(m.Minutes()))
 	seconds := int(math.Floor(m.Seconds())) - (minutes * 60)
 	return fmt.Sprintf("%dm%ds", minutes, seconds)
+}
+
+type config struct {
+	Encoder string `json:"encoder"`
+	Quality int    `json:"quality"`
+}
+
+// Creates a config file with all global defaults. The file will be written to the current working directory.
+func createConfigFile() error {
+	var exists bool = true
+
+	fileInfo, err := os.Stat(fmt.Sprintf("./%s", CONFIG_FILE_NAME))
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			exists = false
+		} else {
+			fmt.Printf("An error occurred while scanning for a preexisting %s file.\n", CONFIG_FILE_NAME)
+			return err
+		}
+	}
+
+	if exists {
+		fmt.Printf("Skipping creation of config file. The file %s already exists.\n", fileInfo.Name())
+		return nil
+	}
+
+	defaultConfig := config{
+		Encoder: DEFAULT_ENCODER,
+		Quality: DEFAULT_QUALITY,
+	}
+
+	defaultConfigBytes, err := json.Marshal(&defaultConfig)
+
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(CONFIG_FILE_NAME, defaultConfigBytes, 0750)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Reads the config file and returns a config struct.
+// If the config file does not exist, it returns a config with global defaults.
+func readConfig() (*config, error) {
+	filePath := fmt.Sprintf("./%s", CONFIG_FILE_NAME)
+
+	// Check if the file exists
+	if _, err := os.Stat(filePath); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			// Config file does not exist, return default config
+			return &config{
+				Encoder: DEFAULT_ENCODER,
+				Quality: DEFAULT_QUALITY,
+			}, nil
+		}
+		// Some other error occurred
+		return nil, fmt.Errorf("error checking config file: %w", err)
+	}
+
+	// Read the file
+	fileData, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	// Unmarshal the file data into the config struct
+	var cfg config
+	err = json.Unmarshal(fileData, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing config file: %w", err)
+	}
+
+	return &cfg, nil
 }
 
 // Takes in the list of directory names to create. Returns a full path to the input file and a full path to the output file.
@@ -225,4 +440,20 @@ func createDirectories(dirNames ...string) error {
 	}
 
 	return nil
+}
+
+// Reads the size of the specified file and returns it in bytes.
+// If the file does not exist or an error occurs, it returns an error.
+func getFileSize(filePath string) (int64, error) {
+	// Get file info using os.Stat
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return 0, fmt.Errorf("file does not exist: %s", filePath)
+		}
+		return 0, fmt.Errorf("error retrieving file info: %w", err)
+	}
+
+	// Return the size of the file
+	return fileInfo.Size(), nil
 }
