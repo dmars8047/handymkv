a08f2b558e9f93ad349030af46313660143d40f0
Daniel Marshall <dmars@raichu.bearded-piano.ts.net>
2025-07-07 18:13:08 -0400

    updating version number

--- cmd/handymkv/main.go
+++ cmd/handymkv/main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/dmars8047/handymkv/internal/hmkv"
 )
 
-const applicationVersion = "0.1.15"
+const applicationVersion = "0.1.16"
 
 func main() {
 	// Parse command line args
